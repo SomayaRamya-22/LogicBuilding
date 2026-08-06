07f51aa99cdb9a1db93d25dc56c6684dde5cc2f7
SomayaRamya-22 <somayaramyam@gmail.com>
2026-08-06 19:47:30 +0530

    JavaPrograms

---
+++ DecimalToBinary.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int num = sc.nextInt();
+
+        if (num == 0) {
+            System.out.println("Binary: 0");
+            return;
+        }
+
+        String binary = "";
+
+        while (num > 0) {
+            binary = (num % 2) + binary;
+            num = num / 2;
+        }
+
+        System.out.println("Binary: " + binary);
+    }
+}
